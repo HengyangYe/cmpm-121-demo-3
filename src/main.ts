27b93f9b7aa8eae25cf0ec9a953e712b4c338d43
HengyangYe <heye@ucsc.edu>
2024-11-10 15:27:41 -0800

    L16

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
 // todo
+// Button
+const button = document.createElement('button');
+
+button.textContent = 'Click Me';
+
+button.addEventListener('click', () => {
+  alert('You clicked the button!');
+});
+
+document.body.appendChild(button);
